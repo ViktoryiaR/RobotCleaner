2292d93b46d38318f4a45700ec767ac6269239fa
Viktoryia Rymkevich <viktoryiar@compatibl.com>
2020-02-24 17:11:10 +0100

    Support console input/output

--- RobotCleaner/Program.cs
+++ RobotCleaner/Program.cs
@@ -6,7 +6,29 @@ namespace RobotCleaner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numberOfCommands = int.Parse(Console.ReadLine());
+            var startingPoint = ParseStartingPoint(Console.ReadLine());
+
+            var commands = new (char c, int s)[numberOfCommands];
+            for (var i = 0; i < numberOfCommands; i++)
+            {
+                commands[i] = ParseCommand(Console.ReadLine());
+            }
+
+            var result = RobotCleaner.Clean(startingPoint, commands);
+            Console.WriteLine($"=> Cleaned: {result}");
+        }
+
+        private static (int x, int y) ParseStartingPoint(string input)
+        {
+            var args = input.Split(' ');
+            return (x: int.Parse(args[0]), y: int.Parse(args[1]));
+        }
+
+        private static (char c, int s) ParseCommand(string input)
+        {
+            var args = input.Split(' ');
+            return (c: char.Parse(args[0]), s: int.Parse(args[1]));
         }
     }
 }
