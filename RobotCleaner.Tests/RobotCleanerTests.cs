d112902a4cd390c1a9e43bc200d58aba68848df8
Viktoryia Rymkevich <viktoryiar@compatibl.com>
2020-02-24 08:56:39 +0100

    Initial implementation

---
+++ RobotCleaner.Tests/RobotCleanerTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace RobotCleaner.Tests
+{
+    public class RobotCleanerTests
+    {
+        [Fact]
+        public void TestZeroCommands()
+        {
+            var result = RobotCleaner.Clean((x: 0, y: 0), new (char c, int s)[0]);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void TestNotOverlappingCommands()
+        {
+            var result = RobotCleaner.Clean((x: 0, y: 0), new []
+            {
+                (c: 'E', s: 2),
+                (c: 'N', s: 1)
+            });
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void TestOverlappingCommands()
+        {
+            var result = RobotCleaner.Clean((x: 0, y: 0), new[]
+            {
+                (c: 'E', s: 2),
+                (c: 'W', s: 1)
+            });
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ RobotCleaner/RobotCleaner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RobotCleaner
+{
+    public static class RobotCleaner
+    {
+        public static int Clean((int x, int y) startingPoint, (char c, int s)[] commands)
+        {
+            var cleanedPlaces = new HashSet<(int x, int y)>
+            {
+                startingPoint
+            };
+
+            var currentPoint = startingPoint;
+            foreach (var command in commands)
+            {
+                Func<(int x, int y), (int x, int y)> getNextPointFunc;
+                switch (command.c)
+                {
+                    case 'E':
+                        getNextPointFunc = (p) => (x: p.x + 1, y: p.y);
+                        break;
+
+                    case 'W':
+                        getNextPointFunc = (p) => (x: p.x - 1, y: p.y);
+                        break;
+
+                    case 'S':
+                        getNextPointFunc = (p) => (x: p.x, y: p.y - 1);
+                        break;;
+
+                    case 'N':
+                        getNextPointFunc = (p) => (x: p.x, y: p.y + 1);
+                        break;
+
+                    default:
+                        throw new ArgumentException();
+                }
+
+                for (var i = 0; i < command.s; i++)
+                {
+                    currentPoint = getNextPointFunc(currentPoint);
+                    cleanedPlaces.Add(currentPoint);
+                }
+            }
+
+            return cleanedPlaces.Count;
+        }
+
+    }
+}
